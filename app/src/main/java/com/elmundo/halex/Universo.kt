a28cad39d851e07578b3feef1965ff0f5885b352
IsraelAIM <75389817+IsraelAIM@users.noreply.github.com>
2022-03-14 20:40:15 -0500

    Primer paso

---
+++ app/src/main/java/com/elmundo/halex/Universo.kt
@@ -0,0 +1,11 @@
+package com.elmundo.halex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Universo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_universo)
+    }
+}
